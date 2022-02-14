034b4f8b4c401a91027043ba9852290f060ac9b3
devin <devinreina99@gmail.com>
2022-02-13 23:36:25 -0800

    added questions and validation

--- app.js
+++ app.js
@@ -1,14 +1,161 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name? (Required)',
+                validate: nameInput => {
+                    if(nameInput)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        console.log('Please enter your name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your GitHub Username',
+                validate: gitInput => {
+                    if(gitInput)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        console.log('Please enter your GitHub username!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAbout',
+                message: 'Would you like to enter some information about yourself for an "About" secction?',
+                default: true  
+            },
+            {
+                type: 'input',
+                name: 'about',
+                message: 'Provide some information about yourself',
+                when: ({ confirmAbout }) => {
+                    if(confirmAbout)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        ]);
+};
+
+const promptProject = portfolioData => {
+    if(!portfolioData.projects)
+    {
+        portfolioData.projects = [];
+    }
+    
+    console.log(`
+    ==================
+    Add a New Project
+    ==================
+    `);
+    return inquirer.prompt([
         {
-          type: 'input',
-          name: 'name',
-          message: 'What is your name?'  
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            validate: projectInput => {
+                if(projectInput)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: desc => {
+                if(desc)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: projectLink => {
+                if(projectLink)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter your project link!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if(projectData.confirmAddProject)
+        {
+            return promptProject(portfolioData);
+        }
+        else
+        {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+   .then(promptProject)
+   .then(portfolioData => {
+       console.log(portfolioData);
+   });
 /*const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
